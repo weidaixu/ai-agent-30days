d1ee15b1e5d01168cd8b7658700a824f0392b015
weidaixu <2199432927@qq.com>
2026-09-10 21:59:57 +0800

    day01 complete

---
+++ day01/pratice.py
@@ -0,0 +1,22 @@
+import json
+
+with open("jobs.json", "r", encoding="utf-8") as f:
+    jobs_data = json.load(f)
+
+
+def filter_jobs(min_salary, target_city):
+    found = False
+
+    for job in jobs_data:
+        if job["salary"] >= min_salary and job["city"] == target_city:
+            print(job["name"], job["salary"], job["city"])
+            found = True
+
+    if found == False:
+        print("没有找到符合的岗位")
+
+
+min_salary = int(input("请输入最低工资："))
+target_city = input("请输入目标城市：")
+
+filter_jobs(min_salary, target_cityiz)   
